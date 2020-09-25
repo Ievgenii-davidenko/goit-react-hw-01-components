cf1bb4dd212e4c7bdcc55b572ca710e727c22445
Ievgenii <bijibnui88@gmail.com>
2020-09-25 12:01:45 +0300

    added index.html

---
+++ build/precache-manifest.186ec6943a28bce638be0bef438bf5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96513127c5e6025a6be56f45b92a78b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "86f69ab0f4802d64bf95",
+    "url": "/goit-react-hw-01-components/static/css/main.9a41ae7f.chunk.css"
+  },
+  {
+    "revision": "c9b4603ab169b6a847c3",
+    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f69ab0f4802d64bf95",
+    "url": "/goit-react-hw-01-components/static/js/main.46b5f91c.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
