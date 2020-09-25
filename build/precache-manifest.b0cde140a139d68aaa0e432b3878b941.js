6e74204630b22f7ac0685973969441b9f7e96a21
Ievgenii <bijibnui88@gmail.com>
2020-09-25 22:02:06 +0300

    added index.html

--- build/precache-manifest.b0cde140a139d68aaa0e432b3878b941.js
+++ build/precache-manifest.b0cde140a139d68aaa0e432b3878b941.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "86f69ab0f4802d64bf95",
-    "url": "/goit-react-hw-01-components/static/css/main.9a41ae7f.chunk.css"
+    "url": "/goit-react-hw-01-components/build/static/css/main.9a41ae7f.chunk.css"
   },
   {
     "revision": "c9b4603ab169b6a847c3",
-    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js"
+    "url": "/goit-react-hw-01-components/build/static/js/2.7ed1cf1c.chunk.js"
   },
   {
     "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
-    "url": "/goit-react-hw-01-components/static/js/2.7ed1cf1c.chunk.js.LICENSE.txt"
+    "url": "/goit-react-hw-01-components/build/static/js/2.7ed1cf1c.chunk.js.LICENSE.txt"
   },
   {
     "revision": "86f69ab0f4802d64bf95",
-    "url": "/goit-react-hw-01-components/static/js/main.46b5f91c.chunk.js"
+    "url": "/goit-react-hw-01-components/build/static/js/main.46b5f91c.chunk.js"
   },
   {
     "revision": "5529903743abc23e6f33",
-    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+    "url": "/goit-react-hw-01-components/build/static/js/runtime-main.5c5371af.js"
   }
 ]);
