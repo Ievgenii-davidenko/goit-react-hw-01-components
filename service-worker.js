df16caeba7bf3e25e80639b6c2f983400e046ca8
Ievgenii <bijibnui88@gmail.com>
2020-09-25 13:38:50 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.186ec6943a28bce638be0bef438bf5ef.js"
+  "/goit-react-hw-01-components/precache-manifest.b0cde140a139d68aaa0e432b3878b941.js"
 );
 
 self.addEventListener('message', (event) => {
